92bd54c3c25bc0add26df07a1cb75222e2c66b9c
Tiunn Ik-Tsin <tiunniktsin@gmail.com>
2024-09-24 15:12:47 +0800

    Add some comments in main file

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-
+# Tell me whether the bot is online
 @bot.event
 async def on_ready():
   print(f"Logged in as {bot.user.name}")
 
-
+# Get token
 with open("token.txt") as file:
   token = file.read()
 
-
+# Read thru all the py files in the folder
 async def Load():
   for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
       await bot.load_extension(f'cogs.{filename[:-3]}')
 
-
+# Run main script
 async def main():
   async with bot:
     await Load()
