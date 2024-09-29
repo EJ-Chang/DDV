11c1a12191dfac72a07624787c9f79e4ccd61bf8
Tiunn Ik-Tsin <tiunniktsin@gmail.com>
2024-09-29 21:17:41 +0800

    change to channel info

--- cogs/twitch_info.py
+++ cogs/twitch_info.py
@@ -51,7 +51,8 @@ class Twitch_info(commands.Cog):
 
   # Get current stream info (title, etc.)
   async def get_twitch_stream_info(self, user_id):
-    url = f'https://api.twitch.tv/helix/streams?user_id={user_id}'
+    # url = f'https://api.twitch.tv/helix/streams?user_id={user_id}'
+    url = f'https://api.twitch.tv/helix/chnnels?broadcaster_id={user_id}'
     headers = {
         'Client-ID': TWITCH_CLIENT_ID,
         'Authorization': f'Bearer {TWITCH_TOKEN}'
@@ -70,24 +71,14 @@ class Twitch_info(commands.Cog):
       return None
 
   @commands.command()
-<<<<<<< HEAD
-  async def viewcarry(self, ctx):
-    user_name = "carrymaybe484"  # Twitch user ID
-=======
   async def view(self, ctx):
     user_name = "v4181"  # Twitch ID
->>>>>>> c90a018eafaa40e266b2dedc58962d3fd515c402
     user_info = await self.get_twitch_user_info(user_name)
 
     if user_info:
       user_id = user_info['id']
       avatar_url = user_info['profile_image_url']
 
-      # # 獲取排程資訊
-      # schedule_info = await self.get_twitch_schedule(user_id)
-
-      # embed = discord.Embed(title=f"{user_name}'s Info")
-      # embed.set_image(url=avatar_url)
       # 獲取當前直播資訊
       stream_info = await self.get_twitch_stream_info(user_id)
 
@@ -104,20 +95,6 @@ class Twitch_info(commands.Cog):
       await ctx.send(embed=embed)
     else:
       await ctx.send(f"Could not retrieve info for {user_name}.")
-
-    # if schedule_info:
-    #   # 格式化排程資訊
-    #   schedule_text = "\n".join(f"{item['start_time']} - {item['title']}"
-    #                             for item in schedule_info)
-    #   embed.add_field(name="Upcoming Schedule",
-    #                   value=schedule_text or "No upcoming streams.")
-    # else:
-    #   embed.add_field(name="Upcoming Schedule",
-    #                   value="Could not retrieve schedule.")
-
-    # await ctx.send(embed=embed)
-    # else:
-    #   await ctx.send(f"Could not retrieve info for {user_name}.")
 
 
 async def setup(bot):
