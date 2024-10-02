488eacd9e4ed1f2d1647f2d1531486cde722e35b
Tiunn Ik-Tsin <tiunniktsin@gmail.com>
2024-10-02 10:04:25 +0000

    reply id

--- cogs/dc_handler.py
+++ cogs/dc_handler.py
@@ -20,14 +20,18 @@ class Dc_handler(commands.Cog):
   async def fetch_reply(self, ctx):
     # 檢查是否有回覆某條消息
     if ctx.message.reference:
-        # 抓取被回覆的消息的 ID
-        replied_message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-        
-        # 發送回覆消息的內容和發送時間
-        await ctx.send(f"Replied message content: {replied_message.content}\nSent at: {replied_message.created_at}")
-    else:
-        await ctx.send("You did not reply to any message.")
+      # 抓取被回覆的消息的 ID
+      replied_message = await ctx.channel.fetch_message(
+          ctx.message.reference.message_id)
 
-        
+      # 發送回覆消息的內容和發送時間
+      await ctx.send(
+          f"Replied message content: {replied_message.content}\nSent at: {replied_message.created_at}"
+      )
+      await ctx.send(f"Replied message ID: {replied_message.id}")
+    else:
+      await ctx.send("You did not reply to any message.")
+
+
 async def setup(bot):
   await bot.add_cog(Dc_handler(bot))
