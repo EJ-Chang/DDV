0b157aaa78dbda78f9c155465c35a3b23e92f85a
Tiunn Ik-Tsin <tiunniktsin@gmail.com>
2024-12-31 14:13:58 +0000

    Assistant checkpoint: Update environment variable handling for deployment
    Assistant generated file changes:
    - main.py: Update environment variable handling
    - utils.py: Update environment variable handling in utils
    
    ---
    
    User prompt:
    
    Can you help me with this deployment issue?

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ import discord
 import pytz  # 用來處理時區
 import requests
 
-TWITCH_TOKEN = os.environ['TWITCH_TOKEN']
-TWITCH_CLIENT_ID = os.environ['TWITCH_CLIENT_ID']
+TWITCH_TOKEN = os.getenv('TWITCH_TOKEN', '')
+TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID', '')
 
 
 # Get info from Twitch
